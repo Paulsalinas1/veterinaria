f95363ca6d04770830ca7053b600d3680c573657
CETECOM <cetecom_conce@duoc.cl>
2024-04-26 12:11:54 -0400

    mi primer formulario validado mas menos con scrips

---
+++ js/login.js
@@ -0,0 +1,17 @@
+function validar() {
+    let correo = document.getElementById("idcorreo").value.trim(); // Elimina espacios en blanco al inicio y al final
+    
+    // Expresión regular para validar el formato del correo electrónico
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (correo === "") {
+        document.getElementById("mensaje20").className = "text-danger";
+        document.getElementById("mensaje20").innerHTML = "Debe ingresar el correo electrónico ❌";
+    } else if (!emailRegex.test(correo)) {
+        document.getElementById("mensaje20").className = "text-danger";
+        document.getElementById("mensaje20").innerHTML = "El correo electrónico no es válido ❌";
+    } else {
+        document.getElementById("mensaje20").className = "text-success";
+        document.getElementById("mensaje20").innerHTML = "Correo electrónico válido ✅";
+    }
+}
