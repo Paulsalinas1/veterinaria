548c114b66e07b804e6779b1be7225bd13ea8d94
Paulsalinas1 <paul.salinasc@duocuc.cl>
2024-04-30 14:44:07 -0400

    areglando el login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-function validar() {
+/*function validar() {
     let correo = document.getElementById("idcorreo").value.trim(); // Elimina espacios en blanco al inicio y al final
     
     // Expresión regular para validar el formato del correo electrónico
@@ -15,7 +15,7 @@ function validar() {
         document.getElementById("mensaje20").innerHTML = "Correo electrónico válido ✅";
     }
 }
-
+*/
 //Función para mostrar contraseña de usuarios
 function togglePassword() {
     var input = document.getElementById("contraseñaCreada");
@@ -25,3 +25,59 @@ function togglePassword() {
         input.type = "password";
     }
 }
+
+function validarCorreosuario(){
+    var correo = document.getElementById("correoCreado").value;
+    var mensajeError = document.getElementById("mensajeErrorCorreoCreado");
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (correo === "") {
+        mensajeError.innerText = "Por favor, ingrese su correo.";
+        mensajeError.style.color = "red";
+        return false; // Evita que se envíe el formulario si el correo está vacío
+    }else if (!emailRegex.test(correo)) {
+        mensajeError.innerText = "Ingrese un correo válido.";
+        mensajeError.style.color = "red";
+        return false; // Evita que se envíe el formulario si el correo no contiene "@"
+    }else {
+        mensajeError.innerText = "Correo electrónico válido ✅";
+        mensajeError.style.color = "green";  
+    }
+} 
+function validarContraseñaUsuario(){
+    var contraseñacreada = document.getElementById("contraseñaCreada").value;
+    var mensajeError = document.getElementById("mensajeErrorContraseñaCreada");
+
+    if (contraseñacreada === "") {
+        mensajeError.innerText = "Ingrese su contraseña.";
+        mensajeError.style.color = "red";
+    
+    }else {
+        mensajeError.innerText = "Contraseña correcta ✅";
+        mensajeError.style.color = "green";  
+    }
+}
+
+function validarInicioS2(){
+    validarCorreosuario()
+    validarContraseñaUsuario()
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
