c3a95e5d35aa0ba9b95ad8336b35051a25ad6811
Paulsalinas1 <paul.salinasc@duocuc.cl>
2024-05-07 18:28:07 -0400

    a

--- js/reco.js
+++ js/reco.js
@@ -26,22 +26,27 @@ function validarCorreo() {
     }
 }
 
+function a(){
+    validarCorreo();
+
+}
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
-
+  
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-
+  
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-
-            form.classList.add('was-validated')
-        }, false)
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
     })
-})()
+  })()
