f0d9e951a58661e953c4bec4ea8109472be746cc
Paulsalinas1 <paul.salinasc@duocuc.cl>
2024-05-09 18:37:20 -0400

    adm_tienda_jq

---
+++ js/adm_tienda_jq.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function () {
+    // Manejar el evento click del botón de inicio de sesión
+    $("#valorp_1").attr("onkeypress","soloNumeros2(event)");
+    $("#valorp_2").attr("onkeypress","soloNumeros2(event)");
+});
+
