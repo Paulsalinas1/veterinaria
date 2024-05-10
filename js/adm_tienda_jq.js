d6f490b78d9894dd8c29845b79e4ff34650e4020
Paulsalinas1 <paul.salinasc@duocuc.cl>
2024-05-10 01:39:57 -0400

    imj cambios

--- js/adm_tienda_jq.js
+++ js/adm_tienda_jq.js
@@ -4,5 +4,8 @@ $(document).ready(function () {
     // Manejar el evento click del botón de inicio de sesión
     $("#valorp_1").attr("onkeypress","soloNumeros2(event)");
     $("#valorp_2").attr("onkeypress","soloNumeros2(event)");
+    $("#modi_p1").attr("accept","image/*");
+    $("#modi_p2").attr("accept","image/*");
+    
 });
 
